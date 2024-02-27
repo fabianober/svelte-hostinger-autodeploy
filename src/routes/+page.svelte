0a2a4f25e346167a6919a27edf92639db4cb1044
Fabian Ober <fabian@familie-ober.eu>
2024-02-27 14:52:58 +0100

    add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,8 @@
 </script>
 
 <h1>Welcome to my first Svelte-Page.</h1>
-<button on:click={increment}>Clicked {counter} times</button>
+<button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" on:click={increment}>Clicked {counter} times</button>
+
 
 <br><br>
 <a href="/about">Go to about</a>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
 	kit: {
@@ -11,5 +12,6 @@ export default {
 			precompress: false,
 			strict: true
 		})
-	}
+	},
+	preprocess: vitePreprocess(),
 };
