b13e072df508f3e05f74f29c0d6886d19a662a7d
Fabian Ober <fabian@familie-ober.eu>
2024-02-27 14:37:40 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let counter = 0;
+
+    function increment() {
+        counter += 1;
+    }
+
+</script>
+
+<h1>Welcome to my first Svelte-Page.</h1>
+<button on:click={increment}>Clicked {counter} times</button>
+
+<br><br>
+Go to <a href="https://fabian-ober.de">Fabian Ober</a> to see more.
