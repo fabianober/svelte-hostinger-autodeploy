c79c8a6deb3136fb12c9bc6b260c3bbe852ddb23
Fabian Ober <fabian@familie-ober.eu>
2024-02-27 15:35:57 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 
 </script>
 
-<h1>Welcome to my first Svelte-Page.</h1>
+<h1>Welcome to my first Svelte-Page.</h1><br>
 <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" on:click={increment}>Clicked {counter} times</button>
 
 
