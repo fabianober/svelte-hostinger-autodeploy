9a5c6e77a75702d7281e9330e8b998ae201310f5
Fabian Ober <fabian@familie-ober.eu>
2024-02-27 15:23:46 +0100

    final edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,8 @@
 <a href="/blog/2">Go to blog 2</a><br>
 <a href="/blog/3">Go to blog 3</a><br>
 <br>
+<h1>Go to any blog you like</h1><br>
 <input bind:value={userInput} placeholder="Enter text" />
-<button on:click={handleSubmit}>Submit</button>
+<button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" on:click={handleSubmit}>Go to any blog you like</button>
 <br><br>
 Go to <a href="https://fabian-ober.de">Fabian Ober</a> to see more.
